e7b9f79bfa5dc1f35dd1d055f14256cbbff13ac4
Paul Mateos <mateosau@yahoo.com>
2020-04-21 14:28:35 +1000

    Adding loop

--- HelloWorld/Main.java
+++ HelloWorld/Main.java
@@ -4,6 +4,18 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world");
+		  int counter = 100;
+      for (int i = 0; i < counter; i++) {
+        try {
+          Thread.sleep(5000);
+          System.out.println("Hello world: " + i);
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+          // handle the exception...        
+          // For example consider calling Thread.currentThread().interrupt(); here.
+      }
+			  
+		  }
+	
     }
 }
